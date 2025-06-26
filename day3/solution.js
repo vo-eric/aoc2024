a0d2dd7245691363a83e52d4040cf7743e63d6c3
Eric Vo <vo.eric@gmail.com>
2025-06-26 13:09:42 -0400

    Add day 3 par 2

--- day3/solution.js
+++ day3/solution.js
@@ -5,19 +5,49 @@ var input = fs.readFileSync('input.txt', 'utf8');
 //PART 1
 //Time complexity: O(n) where n is the number of characters in the input
 //Space complexity: O(m) where m is the number of operations from the input
+// const solution = (input) => {
+//   const regex = /mul\(\d+,\d+\)/g;
+//   let result = 0;
+
+//   const operations = input
+//     .match(regex)
+//     .map((op) => op.replace('mul(', '').replace(')', ''));
+
+//   for (const operation of operations) {
+//     const [val1, val2] = operation.split(',');
+//     result += Number(val1) * Number(val2);
+//   }
+
+//   return result;
+// };
+
+//PART 2
 const solution = (input) => {
-  const regex = /mul\(\d+,\d+\)/g;
   let result = 0;
+  const regex = /(mul\(\d+,\d+\)|do\(\)|don't\(\))/g;
 
   const operations = input
     .match(regex)
-    .map((op) => op.replace('mul(', '').replace(')', ''));
+    .map((op) => op.replace('mul(', '').replace(')', '').replace('(', ''));
+
+  let allowed = true;
 
   for (const operation of operations) {
-    const [val1, val2] = operation.split(',');
-    result += Number(val1) * Number(val2);
-  }
+    if (operation === "don't") {
+      allowed = false;
+      continue;
+    }
 
+    if (operation === 'do') {
+      allowed = true;
+      continue;
+    }
+
+    if (allowed) {
+      const [val1, val2] = operation.split(',');
+      result += Number(val1) * Number(val2);
+    }
+  }
   return result;
 };
 
