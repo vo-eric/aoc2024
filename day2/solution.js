8a7f81b82a3a76a00c369432e71f0500e787337f
Eric Vo <vo.eric@gmail.com>
2025-06-26 12:35:56 -0400

    Add day 2 part 2

--- day2/solution.js
+++ day2/solution.js
@@ -3,12 +3,55 @@ var input = fs.readFileSync('input.txt', 'utf8');
 var parsedInput = input.split(/\r?\n/);
 
 // PART 1
-const isMonotonic = (nums) => {
+// const isMonotonic = (nums) => {
+//   const direction = nums[1] - nums[0];
+
+//   for (let i = 1; i < nums.length; i++) {
+//     const currentDirection = nums[i] - nums[i - 1];
+
+//     if (direction * currentDirection < 0) {
+//       return false;
+//     }
+
+//     if (nums[i] === nums[i - 1]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// };
+
+// const solution = (parsedInput) => {
+//   let count = 0;
+//   for (const line of parsedInput) {
+//     let isValid = true;
+//     const split = line.split(' ');
+//     if (!isMonotonic(split)) {
+//       continue;
+//     }
+
+//     for (let i = 0; i < split.length; i++) {
+//       const distance = Math.abs(split[i] - split[i + 1]);
+//       if (distance > 3) {
+//         isValid = false;
+//         break;
+//       }
+//     }
+//     if (isValid) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// };
+
+//PART 2
+
+const lineIsValid = (nums) => {
   const direction = nums[1] - nums[0];
 
   for (let i = 1; i < nums.length; i++) {
     const currentDirection = nums[i] - nums[i - 1];
-
     if (direction * currentDirection < 0) {
       return false;
     }
@@ -16,33 +59,38 @@ const isMonotonic = (nums) => {
     if (nums[i] === nums[i - 1]) {
       return false;
     }
-  }
 
+    if (Math.abs(nums[i] - nums[i - 1]) > 3) {
+      return false;
+    }
+  }
   return true;
 };
 
 const solution = (parsedInput) => {
-  let count = 0;
+  let result = 0;
   for (const line of parsedInput) {
-    let isValid = true;
     const split = line.split(' ');
-    if (!isMonotonic(split)) {
-      continue;
-    }
-
     for (let i = 0; i < split.length; i++) {
-      const distance = Math.abs(split[i] - split[i + 1]);
-      if (distance > 3) {
-        isValid = false;
+      const valid = lineIsValid(split.map(Number));
+
+      if (valid) {
+        result++;
+        break;
+      }
+
+      const validAfterRetry = lineIsValid(
+        split.slice(0, i).concat(split.slice(i + 1))
+      );
+
+      if (validAfterRetry) {
+        result++;
         break;
       }
     }
-    if (isValid) {
-      count++;
-    }
   }
 
-  return count;
+  return result;
 };
 
 console.log(solution(parsedInput));
