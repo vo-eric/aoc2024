dd4f817f11d611151aae4c38ab220821e8baf2b7
Eric Vo <vo.eric@gmail.com>
2025-06-30 09:40:29 -0400

    Add longest subsequence

--- leetcode.js
+++ leetcode.js
@@ -24,3 +24,31 @@ function lengthOfLongestSubstring(s: string): number {
 
   return result;
 }
+
+/**
+ * https://leetcode.com/problems/longest-consecutive-sequence/?envType=study-plan-v2&envId=top-interview-150
+ * Time Complexity: O(n) where n is the largest number in the array
+ * Space Complexity: O(m) where m is the number of elements
+ */
+
+function longestConsecutive(nums: number[]): number {
+  const seen: Set<number> = new Set(nums);
+  let result: number = 0;
+
+  for (const num of nums) {
+    let current = num;
+    let count = 1;
+
+    if (seen.has(num + 1)) {
+      continue;
+    }
+
+    while (seen.has(--current)) {
+      count++;
+    }
+
+    result = Math.max(result, count);
+  }
+
+  return result;
+}
